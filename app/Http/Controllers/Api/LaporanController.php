b44831acf670cd110db4774865edbb784c390e2d
muhilman2012 <muhi7714@gmail.com>
2025-01-03 09:03:55 +0700

    Change Required KK

--- app/Http/Controllers/Api/LaporanController.php
+++ app/Http/Controllers/Api/LaporanController.php
@@ -68,7 +68,7 @@ class LaporanController extends Controller
                 ],
                 'tanggal_kejadian' => 'nullable|date_format:d/m/Y',
                 'dokumen_ktp' => 'required|url', // KTP harus berupa URL yang valid
-                'dokumen_kk' => 'required|url', // KK harus berupa URL yang valid
+                'dokumen_kk' => 'nullable|url', // KK harus berupa URL yang valid
                 'dokumen_skuasa' => 'nullable|url', // Opsional, harus berupa URL yang valid
                 'dokumen_pendukung' => 'required|url', // Harus berupa URL yang valid
                 'nomor_pengadu' => 'nullable|string|max:15',
